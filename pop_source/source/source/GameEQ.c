5f73e528bbf7a2c1938d7d8933f7537df030af6c
FatiH2O <101948894+FatiH2O@users.noreply.github.com>
2025-05-08 16:29:35 +0200

    Delete pop_source/source/source directory

--- pop_source/source/source/GameEQ.c
+++
@@ -1,8 +0,0 @@
-#include "GameEQ.h"
-
-int BGset1=0;
-int doortop;
-int KidBlockY;
-int KidBlockX;
-int KidScrn;
-int scrnRight;
--- pop_source/source/source/GameEQ.h
+++
@@ -1,9 +0,0 @@
-#pragma once 
-#define UseLay 1
-
-extern int BGset1;
-extern int doortop;
-extern int KidBlockY;
-extern int KidBlockX;
-extern int KidScrn;
-extern int scrnRight;
